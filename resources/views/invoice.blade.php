0a923d0c86e9ef6d2ca32b3ac432f301e5532c6a
Mohazm <mohamadalit@gmail.com>
2023-07-19 11:26:09 +0700

    update

--- resources/views/invoice.blade.php
+++ resources/views/invoice.blade.php
@@ -6,18 +6,59 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="invoice.css">
 </head>
 
 <body>
-
+<button type="button" class="btn btn-link"> <a href="mailto:mohamadalit24@gmail.com?subject= Saran : .......">Saran & Masukkan</a></button>
+<button type="button" class="btn btn-link" onclick="printStruk()">Cetak</button>
+<button type="button" class="btn btn-link"><a href="/" >Kembali</a></button>
     <div class="d-flex justify-content-center mt-5">
-        <div class="card" style="width: 18rem;">
+        <div class="card">
             <div class="card-header text-center">
                 Pembayaran Sukses
             </div>
             <div class="card-body">
-                <h5 class="card-title">Detail Pesanan</h5>
+                <h1 class="text-center">Sparepart SPBU</h1>
+                <p class="text-center">Ciracas, Jakarta Timur </p>       
                 <table>
+                    <tr>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+                        </td>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+                        </td>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+                        </td>
+                    </tr>
+                    <tr class="text-end">
+                        <td></td>
+                        <td></td>
+                        <td>
+                            <p>{{$order->created_at}}</p>
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+                        </td>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+
+                        </td>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+
+                        </td>
+                    </tr>
                     <tr>
                         <td>Nama</td>
                         <td> : {{$order->name}}</td>
@@ -32,23 +73,49 @@
                     </tr>
                     <tr>
                         <td>Total Harga</td>
-                        <td> : {{$order->total_price}}</td>
+                        <td> : Rp. {{$order->total_price}}</td>
                     </tr>
                     <tr>
                         <td>Status</td>
                         <td> : {{$order->status}}</td>
                     </tr>
-                    <tr class="mt-2">
-                        <td> <a href="/" class="btn btn-primary mt-2 ">Kembali</a></td>
+                    <tr>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+                        </td>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+
+                        </td>
+                        <td>
+                            <p>=============</p>
+                            <p>=============</p>
+
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td colspan="2"><p>Terima kasih atas pembelian Anda!</p></td>
                     </tr>
             </div>
-
         </div>
+    </div>
 
-    </div>
-    </div>
+
 
 
 </body>
+<!-- Tambahkan di akhir berkas body -->
+<script>
+    window.onload = function() {
+        window.print();
+    };
+    function printStruk() {
+      window.print();
+    }
+
+</script>
 
 </html>
--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -7,7 +7,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>@yield('title')</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-  <link rel="stylesheet" href="{{asset('css/style.css')}}">
+  <link rel="stylesheet" href="../../css/style.css">
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
   <script src="https://cdn.jsdelivr.net/npm/jquery@3.6.0/dist/jquery.min.js"></script>
@@ -206,47 +206,6 @@
 </body>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
 
-<script type="text/javascript">
-   
-    $(".cart_update").change(function (e) {
-        e.preventDefault();
-   
-        var ele = $(this);
-   
-        $.ajax({
-            url: "{{ route('update_cart') }}",
-            method: "patch",
-            data: {
-                _token: '{{ csrf_token() }}', 
-                id: ele.parents("tr").attr("data-id"), 
-                quantity: ele.parents("tr").find(".quantity").val()
-            },
-            success: function (response) {
-               window.location.reload();
-            }
-        });
-    });
-   
-    $(".cart_remove").click(function (e) {
-        e.preventDefault();
-   
-        var ele = $(this);
-   
-        if(confirm("Do you really want to remove?")) {
-            $.ajax({
-                url: "{{ route('remove_from_cart') }}",
-                method: "DELETE",
-                data: {
-                    _token: '{{ csrf_token() }}', 
-                    id: ele.parents("tr").attr("data-id")
-                },
-                success: function (response) {
-                    window.location.reload();
-                }
-            });
-        }
-    });
-   
-</script>
+
 
 </html>
